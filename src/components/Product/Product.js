e22973393526b62b55af9ba9d5ca03dc3bec42d4
happy149 <hosnearahappybd@gmail.com>
2021-02-26 18:30:06 +0600

    34-8

---
+++ src/components/Product/Product.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Product = (props) => {
+    
+    return (
+        <div>
+            
+            <h4>{props.product.name}</h4>
+            
+        </div>
+    );
+};
+
+export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import React, { useState } from 'react';
+import'./Shop.css';
+import Product from '../Product/Product';
+
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products, setProducts] = useState(first10);
     return (
-        <div>
-            <h1>This is Shop</h1>
-            <h3>{products.length}</h3>
-            <ul>{
-                products.map(product=> <li>{product.name}</li>)
+        <div className="shop-container">
+            <div className="product-container">
+            {
+                products.map(product=> <Product product={product}></Product>)
 
 
                 }
-            </ul>
+            
+            </div>
+            <div className="cart-container">
+                <h3>This is cart</h3>
+            </div>
+            
+            
             
         </div>
     );
