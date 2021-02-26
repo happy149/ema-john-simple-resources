7e6ac4431ad0a7f93fe828dcee8c497ebf018da5
happy149 <hosnearahappybd@gmail.com>
2021-02-26 19:21:36 +0600

    more

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import './Product.css';
 
 const Product = (props) => {
+const {img, name, seller, price, stock} = props.product;
     
     return (
-        <div>
+        <div className="product">
+            <div>
+                <img src={img} alt=""/>
+
+            </div>
+            <div> 
+            <h4 className= "product-name">{name}</h4>
+            <br/>
+            <p><small>by: {seller}</small></p>
+            <p>${price}</p>
             
-            <h4>{props.product.name}</h4>
+            <p><small>only {stock} left in stock - order soon </small></p>
+
+            </div>
+            
+           
             
         </div>
     );
