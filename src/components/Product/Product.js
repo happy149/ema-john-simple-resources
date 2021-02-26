7266b1025f8f99f39999c0b3ea2bed08eb165a0e
happy149 <hosnearahappybd@gmail.com>
2021-02-26 21:57:51 +0600

    cat button

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
 import './Product.css';
 
 const Product = (props) => {
+    //console.log(props);
+
 const {img, name, seller, price, stock} = props.product;
     
     return (
@@ -17,6 +22,11 @@ const {img, name, seller, price, stock} = props.product;
             <p>${price}</p>
             
             <p><small>only {stock} left in stock - order soon </small></p>
+            <button 
+            className="main-button"
+            onClick={() => props.handleAddProduct(props.product)}
+            >
+                <FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
 
             </div>
             
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,11 +7,22 @@ import Product from '../Product/Product';
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+
+    const handleAddProduct = (product) => {
+       console.log ('Product added',product);
+       const newCart = [...cart, product];
+       setCart(newCart);
+    }
     return (
         <div className="shop-container">
             <div className="product-container">
             {
-                products.map(product=> <Product product={product}></Product>)
+                products.map(product=> <Product
+                handleAddProduct = {handleAddProduct}
+                     product={product}
+                     ></Product>)
 
 
                 }
@@ -19,6 +30,8 @@ const Shop = () => {
             </div>
             <div className="cart-container">
                 <h3>This is cart</h3>
+                <h5>Order Summary: {cart.length}</h5>
+
             </div>
             
             
