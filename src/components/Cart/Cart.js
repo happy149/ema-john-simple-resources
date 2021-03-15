5aef05664b415695d5ebaa6e7a06000da6ee2aa6
happy149 <hosnearahappybd@gmail.com>
2021-03-15 21:24:58 +0600

    38-4

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,9 +7,10 @@ const Cart = (props) => {
    let total = 0;
    for(let i=0;i< cart.length; i++){
     const product = cart[i];
-    total = total + product.price * product.quantity;    
+    total = total + product.price * product.quantity;  
+    debugger;  
    }
-   
+
     let shipping = 0;
     if(total > 35){
         shipping = 0;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -13,10 +13,24 @@ const Shop = () => {
 
 
     const handleAddProduct = (product) => {
-       const newCart = [...cart, product];
+        const toBeAddedKey = product.key;
+
+        const sameProduct = cart.find(pd=> pd.key === product.key);
+        let count = 1;
+        let newCart;
+        if(sameProduct){
+            count = sameProduct.quantity + 1;
+            sameProduct.quantity = count;
+            const others = cart.filter(product=> product.key !==toBeAddedKey);
+            newCart = [...others, sameProduct];
+        }
+        else{
+            product.quantity =1;
+            newCart = [...cart, product];
+        }
+    
        setCart(newCart);
-       const sameProduct = newCart.filter(pd=> pd.key === product.key)
-       const count = sameProduct.length;
+       
        addToDatabaseCart(product.key, count);
     }
     return (
