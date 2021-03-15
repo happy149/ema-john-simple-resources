58771891eab53b669e032680afd3d057eaed5a79
happy149 <hosnearahappybd@gmail.com>
2021-03-15 20:34:51 +0600

    38-3

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
+import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
     const [cart, setCart] = useState([]);
+   
+
+    const removeProduct = (productKey) => {
+        const newCart = cart.filter(product => product.key !== productKey);
+        setCart(newCart);
+        removeFromDatabaseCart(productKey);
+    }
+
+
     useEffect(() => {
         //cart
         const savedCart = getDatabaseCart();
@@ -18,14 +28,20 @@ const Review = () => {
         setCart(cartProducts);
     },[])
     return (
-        <div>
-            <h1>Cart Items: {cart.length}</h1>
+        <div className="twin-container">
+            <div className="product-container">
             {
                 cart.map(product => <ReviewItem 
                     key={product.key}
+                    removeProduct = {removeProduct}
                     product={product}></ReviewItem>)
             }
 
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+
+            </div>
         </div>
     );
 };
